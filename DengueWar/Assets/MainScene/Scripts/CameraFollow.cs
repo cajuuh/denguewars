873aaaaece28b3d29341c39c9fad583e0af2e4ab
cajuuh <pedro.filipi.alcantara@gmail.com>
2014-08-27 20:08:03 -0300

    controle
    controle.

---
+++ DengueWar/Assets/MainScene/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	float trackingSpeed = 2.0f;
+	float zoomSpeed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		if (player == null)
+			player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = player.position;
+		pos.y = transform.position.y;
+		pos.z = -10;
+
+		transform.position = Vector3.Lerp(transform.position, pos, trackingSpeed * Time.deltaTime);
+		//camera.orthographicSize += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+	}
+}
---
+++ DengueWar/Assets/MainScene/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+	
+	Animator anim;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void FixedUpdate()
+	{
+		float move = Input.GetAxis("Horizontal");
+	}
+
+	void Update () 
+	{
+		
+	}
+}
