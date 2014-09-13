4495352ab4ac26a0d040650f3345a0c2ee7011a3
cajuuh <pedro.filipi.alcantara@gmail.com>
2014-09-13 18:51:25 -0300

    update

--- DengueWar/Assets/MainScene/Scripts/ArrowScript.cs
+++ DengueWar/Assets/MainScene/Scripts/ArrowScript.cs
@@ -4,9 +4,16 @@ using System.Collections;
 public class ArrowScript : MonoBehaviour {
 	
 	public GUITexture touchButton;
-	Touch evt;
+	public GameObject player;
 
 	void Update () 
 	{
+		if(touchButton.HitTest(Input.GetTouch(0).position))
+		{
+			if(Input.GetTouch(0).phase == TouchPhase.Stationary)
+			{
+				//player.rigidbody2D.velocity = (2*5, player.rigidbody2D.velocity.y);
+			}
+		}
 	}
 }
--- DengueWar/Assets/MainScene/Scripts/PlayerControllerScript.cs
+++ DengueWar/Assets/MainScene/Scripts/PlayerControllerScript.cs
@@ -5,6 +5,7 @@ public class PlayerControllerScript : MonoBehaviour {
 
 	public float maxSpeed = 1.2f;
 	public bool facingRight = true;
+	public GUITexture touchButton;
 	int calculator;
 	
 	CNJoystick stick;
@@ -19,6 +20,13 @@ public class PlayerControllerScript : MonoBehaviour {
 	
 	void FixedUpdate()
 	{
+		if(Input.touchCount > 0 && touchButton.HitTest(Input.GetTouch(0).position))
+		{
+			if(Input.GetTouch(0).phase == TouchPhase.Stationary)
+			{
+				print("apertou");
+			}
+		}
 	}
 	void Update () 
 	{
